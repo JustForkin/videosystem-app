393ecc618d07520ad711d48f48eeb99568ce6d33
George <ast809809@gmail.com>
2018-05-09 22:54:08 +0300

    Changed Header a little bit

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,12 +2,18 @@
   <div class="container grid-xl">
     <header class="navbar">
       <section class="navbar-section">
-        <router-link to="/" class="btn btn-link">VIDEOSYSTEM</router-link>
-        <!-- <a href="#" class="btn btn-link">VideoSystem App</a> -->
+        <div class="input-group input-inline">
+          <input class="form-input" type="text" placeholder="Search videos">
+          <button class="btn btn-link input-group-btn">
+            <i class="icon icon-search"></i>
+          </button>
+        </div>
       </section>
-      <section class="navbar-center">
 
+      <section class="navbar-center">
+        <router-link to="/" class="btn btn-link btn-lg">VIDEOSYSTEM</router-link>
       </section>
+
       <section class="navbar-section">
         <router-link
           v-if="!$store.state.isUserLoggedIn"
@@ -17,18 +23,36 @@
           v-if="!$store.state.isUserLoggedIn"
           to="signup"
           class="btn btn-link btn-sm">Sign Up</router-link>
+        <button
+          v-if="$store.state.isUserLoggedIn"
+          @click="logout"
+          class="btn btn-link btn-sm">Log Out</button>
       </section>
+
     </header>
-  </div>
+  </div><!-- container -->
 </template>
 
 <script>
 export default {
+  methods: {
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+
+      // redirect to homepage
+      this.$router.push({
+        name: 'Start'
+      })
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.btn-lg{
+.navbar{
+  padding-bottom: 5px;
+  padding-top: 5px;
 }
 </style>
